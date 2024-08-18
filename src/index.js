28cb988fc7671b8d754d789723a28b5324c60b31
venus-j <chumbavenus24@gmail.com>
2024-08-18 23:56:42 +0300

    router-routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import Home from "./pages/Home";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+// function Routes
+
+const routes = [
+  { path: "/", element: <Home />, errorElement: <ErrorPage /> },
+  { path: "/directors", element: <Directors />, errorElement: <ErrorPage /> },
+  { path: "/actors", element: <Actors />, errorElement: <ErrorPage /> },
+  { path: "/movie/:id", element: <Movie />, errorElement: <ErrorPage /> },
+  { path: "*", element: <ErrorPage /> },
+];
+// export default routes;
+
+export default routes;
